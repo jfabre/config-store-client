5b7fa0324af7a1ef3b8dc47ec74fd78b0c7016fd
Jeremy Fabre <jeremy.fabre@hotmail.com>
2012-11-09 17:32:52 -0500

    Add command

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ group :test do
   gem 'rspec'
 end
 gem 'activeresource', require: 'active_resource'
-
+gem 'json'
 group :development, :production do
   gem 'commander', require: 'commander/import'
 end
--- lib/config_store.rb
+++ lib/config_store.rb
@@ -17,4 +17,31 @@ module ConfigStore
     to_store = {:store => store.id, :org => org.id}.to_json
     File.open(".config-store", 'w') {|f| f.write(to_store) }
   end
+ 
+
+  def self.add(key_values)
+    load_associations
+    pairs = Pair.all(params: Pair.prefix_options)
+    
+    to_create = key_values.select{|k, v| !pairs.any?{|p| p.key == k.to_s } }
+    to_update = pairs.select{|p| key_values.keys.any?{|k| p.key == k.to_s } }
+    
+    to_create.each do |k, v|
+      Pair.create(:key => k, :value => v)
+    end
+    to_update.each { |p| p.save! }
+  end
+
+  def self.load_associations
+    @org ||= Organization.find(store_keys[:org])
+    Store.org = @org
+    @store ||= Store.find(store_keys[:store], params: Store.prefix_options)
+
+    Pair.org = @org
+    Pair.store = @store
+  end
+
+  def self.store_keys
+    @keys ||= File.open(".config-store", 'r') { |f| JSON.parse(f.read).inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo } }
+  end
 end
